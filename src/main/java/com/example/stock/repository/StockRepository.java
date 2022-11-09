f3bcacdeccfcf6af7ba97ea7fd7778f55e9d2f6e
woo00oo <96woo94@naver.com>
2022-11-10 00:13:02 +0900

    라이브러리 장단점, MySQL 과 Redis 비교

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -22,6 +22,9 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 }
 
 /**
+ *
+ * MySQL
+ *
  * 1. Pessimistic Lock
  * - 실제로 데이터에 Lock 을 걸어서 정합성을 맞추는 방법.
  *   exclusive lock 을 걸게되면 다른 트랜잭션에서는 lock 이 해제되기전에 데이터를 가져갈 수 없게 된다.
@@ -40,4 +43,31 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
  *   주의할점은 transaction 이 종료될 때 lock 이 자동으로 해제되지 않는다.
  *   별도의 명령어로 해제를 수행해주거나 선점시간이 끝나야 해제된다.
  *   분산 Lock
+ *
+ * Redis
+ *
+ * 1. Lettuce
+ *  - 구현이 간단한다.
+ *  - Spring data redis 를 이용하면 lettuce 가 기본이기 때문에 별도의 라이브러리를 사용하지 않아도 된다.
+ *  - spin lock 방식이기 때문에 동시에 많은 쓰레드가 lock 획득 대기 상태라면 redis 에 부하가 갈 수 있다.
+ *
+ *
+ * 2. Redisson
+ * - 락 획득 재시도를 기본으로 제공한다.
+ * - pub-sub 방식으로 구현이 되어있기 때문에 lettuce 와 비교했을 때 redis 에 부하가 덜 간다.
+ * - lock 을 라이브러리 차원에서 제공해주기 때문에 사용법을 공부해야 한다.
+ *
+ * 실무에서는 ?
+ * - 재시도가 필요하지 않은 Lock 은 lettuce 활용
+ * - 재시도가 필요한 경우에는 redisson 을 활용
+ *
+ *
+ * MySQL
+ *  - 이미 MySQL을 사용하고 있다면 별도의 비용없이 사용 가능.
+ *  - 어느정도의 트래픽까지는 문제없이 활용 가능.
+ *  - Redis 보다는 성능이 좋지 않다.
+ *
+ *  Redis
+ *  - 활용중인 Redis 가 없다면 별도의 구축비용과 인프라 관리비용이 발생.
+ *  - MySQL 보다 성능이 좋다.
  **/
